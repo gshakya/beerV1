f04f13f599ca1bfbec21150d4d90524a587552c7
Gunjan Shakya <gr.skya@gmail.com>
2016-03-08 22:21:15 -0600

    First Commit

---
+++ src/java/com/ch3/gunjan/model/BeerExpert.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ch3.gunjan.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author grsky
+ */
+public class BeerExpert {
+
+    public List getBrands(String color) {
+        List brands = new ArrayList();
+        if (color == "Amber"){
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+            }
+        else{
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Scout");
+        }
+        return brands;
+    }   
+}
